2dda7f750d07ddd318da6db60bd9d68f1b781c4d
RA-NADILA <ddila929@gmail.com>
2023-06-11 11:23:04 +0700

    update

--- resources/views/user_registers/add.blade.php
+++ resources/views/user_registers/add.blade.php
@@ -17,7 +17,6 @@
                     <form action="{{ route('user_register.store') }}" method="POST">
                         @csrf
 
-
                         <div class="form-group">
                             <label for="name">Nama Lengkap </label>
                             <input type="text" name="name" class="form-control" id="name"
@@ -25,28 +24,70 @@
                         </div>
 
                         <div class="form-group">
-                            <label for="exampleInputNumber1">NIM/NISN</label>
+                            <label for="nisn_nim">NISN/NIM</label>
                             <input type="number" class="form-control" id="nisn_nim" value="{{ old('nisn_nim') }}"
                                 placeholder="NIM/NISN" required>
                         </div>
 
+                        {{-- jenis kelamin --}}
                         <div class="form-group">
-                            <label for="name">Asal Instansi</label>
+                            <label for="gender">Jenis Kelamin</label>
+                            <input type="option" name="gender" class="form-control" id="gender"
+                                value="{{ old('gender') }}" placeholder="Jenis Kelamin" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="birth_date">Tanggal Lahir</label>
+                            <input type="date" name="birth_date" class="form-control" id="birth_date"
+                                value="{{ old('birth_date') }}" placeholder="Tanggal Lahir" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="birth_place">Tempat Lahir</label>
+                            <input type="text" name="birth_place" class="form-control" id="birth_place"
+                                value="{{ old('birth_place') }}" placeholder="Tanggal Lahir" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="address">Alamat</label>
+                            <input type="text" name="address" class="form-control" id="address"
+                                value="{{ old('address') }}" placeholder="Alamat" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="phone_number">No. Handphone</label>
+                            <input type="number" name="phone_number" class="form-control" id="phone_number"
+                                value="{{ old('phone_number') }}" placeholder=" No. Handphone" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="school">Asal Instansi</label>
                             <input type="text" name="school" class="form-control" id="school"
                                 value="{{ old('school') }}" placeholder="Asal Instansi" required>
                         </div>
 
+                        <div class="form-group">
+                            <label for="department">Jurusan </label>
+                            <input type="text" name="department" class="form-control" id="department"
+                                value="{{ old('department') }}" placeholder="Jurusan" required>
+                        </div>
 
                         <div class="form-group">
-                            <label for="exampleInputdate">Tanggal Mulai PKL</label>
+                            <label for="date_start">Tanggal Mulai PKL</label>
                             <input type="date" class="form-control" id="date_start" value="{{ old('date_start') }}"
                                 placeholder="Tanggal Mulai PKL"required>
                         </div>
 
                         <div class="form-group">
-                            <label for="exampleInputdate">Tanggal Selesai PKL</label>
+                            <label for="file">Tanggal Selesai PKL</label>
                             <input type="date" class="form-control" id="date_end" value="{{ old('date_end') }}"
-                                placeholder="Tanggal Mulai PKL"required>
+                                placeholder="Tanggal Selesai PKL"required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="file">File Pengajuan PKL</label>
+                            <input type="file" class="form-control" id="file" value="{{ old('file') }}"
+                                placeholder="Pilih File..."required>
                         </div>
 
 
